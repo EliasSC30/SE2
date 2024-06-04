21c2e61784471a8f3203a56d24adcef8201fcd33
I573040 <elias.rw2@gmail.com>
2024-06-04 18:40:04 +0200

    jo

---
+++ src/test/java/ConverterTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConverterTest {
+
+    @Test
+    void getConversionFactor() {
+    }
+
+    @Test
+    void convertToNeutral() {
+    }
+
+    @Test
+    void convertTo() {
+    }
+
+    @Test
+    void stringToMoneyValue() {
+    }
+
+    @Test
+    void roundTwoPlaces() {
+    }
+}
