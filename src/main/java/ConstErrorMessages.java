855278c5e0b04519e05393a2af74cfc173dd068b
kieberts <sebastian@kiebert.com>
2024-06-22 18:03:51 +0200

    Create FixedExchangeRateProvider Tests

--- src/main/java/ConstErrorMessages.java
+++ src/main/java/ConstErrorMessages.java
@@ -4,10 +4,13 @@ interface ConstErrorMessages {
     String CURRENCIES_NOT_EQUAL = "Currencies are not equal for operation";
     String AMOUNT_NULL = "Amount is null";
     String DIVIDE_BY_ZERO = "Can not divide by zero";
-    // Converter
-    String EXCHANGE_RATE_TYPE_NULL = "ExchangeRate Type is null";
 
     // Calculator
     String MONEY_VALUE_NULL = "Money Value can not be null";
     String CONVERTER_NULL = "Converter can not be null";
+
+    // FixedExchangeRateProvider
+    String EXCHANGE_RATE_TYPE_NULL = "ExchangeRateType can not be null";
+    String CURRENCY_NULL = "Currency can not be null";
+
 }
---
+++ src/test/java/FixedExchangeRateProviderTest.java
@@ -0,0 +1,122 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FixedExchangeRateProviderTest {
+
+    private ExchangeRateProvider exchangeRateProvider;
+
+    @BeforeEach
+    public void init() {
+        exchangeRateProvider = new FixedExchangeRateProvider();
+    }
+
+
+    @Test
+    public void testGetExchangeRateSameCurrencies() {
+        // Given
+        Currency from = Currency.US_DOLLAR;
+        Currency to = Currency.US_DOLLAR;
+        ExchangeRateProvider.ExchangeRateType exchangeRateType = ExchangeRateProvider.ExchangeRateType.REALTIME;
+        double expectedExchangeRate = 1;
+
+        // When
+        double result = exchangeRateProvider.getExchangeRate(from, to, exchangeRateType);
+
+        // Then
+        assertEquals(expectedExchangeRate, result);
+    }
+
+    @Test
+    public void testGetExchangeRateRealTime() {
+       // Given
+       Currency from = Currency.US_DOLLAR;
+       Currency to = Currency.EURO;
+       ExchangeRateProvider.ExchangeRateType exchangeRateType = ExchangeRateProvider.ExchangeRateType.REALTIME;
+       double expectedExchangeRate = 0.93;
+
+       // When
+       double result = exchangeRateProvider.getExchangeRate(from, to, exchangeRateType);
+
+       // Then
+       assertEquals(expectedExchangeRate, result);
+   }
+
+    @Test
+    public void testGetExchangeRateMonthly() {
+        // Given
+        Currency from = Currency.US_DOLLAR;
+        Currency to = Currency.EURO;
+        ExchangeRateProvider.ExchangeRateType exchangeRateType = ExchangeRateProvider.ExchangeRateType.MONTHLY;
+        double expectedExchangeRate = 0.92;
+
+        // When
+        double result = exchangeRateProvider.getExchangeRate(from, to, exchangeRateType);
+
+        // Then
+        assertEquals(expectedExchangeRate, result);
+    }
+
+    @Test
+    public void testGetExchangeRateDaily() {
+        // Given
+        Currency from = Currency.US_DOLLAR;
+        Currency to = Currency.EURO;
+        ExchangeRateProvider.ExchangeRateType exchangeRateType = ExchangeRateProvider.ExchangeRateType.DAILY;
+        double expectedExchangeRate = 0.91;
+
+        // When
+        double result = exchangeRateProvider.getExchangeRate(from, to, exchangeRateType);
+
+        // Then
+        assertEquals(expectedExchangeRate, result);
+    }
+
+    @Test
+    public void testGetExchangeRateTypeNull() {
+        // Given
+        Currency from = Currency.US_DOLLAR;
+        Currency to = Currency.EURO;
+        ExchangeRateProvider.ExchangeRateType exchangeRateType = null;
+
+        // When
+        Exception exception = assertThrows(Exception.class, () -> exchangeRateProvider.getExchangeRate(from, to, exchangeRateType));
+
+        // Then
+        assertEquals(exception.getClass(), RuntimeException.class);
+        assertEquals("ExchangeRateType can not be null", exception.getMessage());
+    }
+
+    @Test
+    public void testGetExchangeRateFromCurrencyNull() {
+        // Given
+        Currency from = null;
+        Currency to = Currency.EURO;
+        ExchangeRateProvider.ExchangeRateType exchangeRateType = ExchangeRateProvider.ExchangeRateType.REALTIME;
+
+        // When
+        Exception exception = assertThrows(Exception.class, () -> exchangeRateProvider.getExchangeRate(from, to, exchangeRateType));
+
+        // Then
+        assertEquals(exception.getClass(), RuntimeException.class);
+        assertEquals("Currency can not be null", exception.getMessage());
+    }
+
+    @Test
+    public void testGetExchangeRateToCurrencyNull() {
+        // Given
+        Currency from = Currency.US_DOLLAR;
+        Currency to = null;
+        ExchangeRateProvider.ExchangeRateType exchangeRateType = ExchangeRateProvider.ExchangeRateType.REALTIME;
+
+        // When
+        Exception exception = assertThrows(Exception.class, () -> exchangeRateProvider.getExchangeRate(from, to, exchangeRateType));
+
+        // Then
+        assertEquals(exception.getClass(), RuntimeException.class);
+        assertEquals("Currency can not be null", exception.getMessage());
+    }
+}
